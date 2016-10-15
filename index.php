c7cdc869029ce703c45b19226340d36a6954394b
PXgamer <owzie123@gmail.com>
2016-10-15 21:39:43 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vidox</title>
+	<link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css" integrity="sha384-MnR/tAdMR2vYfROXmBldczUJ7JqlT7aXOo8b86EdVzdnYU0sJ+0fdXdwmFA5qosE" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body><nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav-collapse" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">vidox</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="main-nav-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/upload/"><span class="glyphicon glyphicon-upload"></span> Upload</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	<div class="container-fluid">
+    <div class="text-center">
+      <button class="btn btn-default">I have an ID</button>
+      <button class="btn btn-default">I'd like to upload</button>
+    </div>
+	</div>
+  </body>
+</html>
